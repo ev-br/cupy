ab0c45093eb903a83adcbc5b83bd38825bd4b729
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2022-11-29 21:18:56 +0300

    TST: test zero-sized arrays for make_interp_spline, too

--- tests/cupyx_tests/scipy_tests/interpolate_tests/test_polyint.py
+++ tests/cupyx_tests/scipy_tests/interpolate_tests/test_polyint.py
@@ -569,23 +569,49 @@ class TestCubicHermiteSpline:
         r = p.roots(extrapolate=True)
         return p.c.shape[1], r.size
 
+
+@testing.with_requires("scipy>=1.10.0")
 class TestZeroSizeArrays:
-    # regression tests for gh-17241 : CubicSpline et al must not segfault
+    # regression tests for SciPy/gh-17241 : CubicSpline et al must not segfault
     # when y.size == 0
     # The two methods below are _almost_ the same, but not quite:
     # one is for objects which have the `bc_type` argument (CubicSpline)
     # and the other one is for those which do not (Pchip, Akima1D)
 
-    # XXX: add back the 2nd test method, w/ make_interp_spline & CubicSpline
+    # XXX: add CubicSpline to the test loop, when implemented
 
     @testing.numpy_cupy_allclose(scipy_name='scp')
-    @pytest.mark.parametrize('y', [xp.zeros((10, 0, 5)),
-                                   xp.zeros((10, 5, 0))])
+    @pytest.mark.parametrize('y_shape', [(10, 0, 5), (10, 5, 0)])
+    @pytest.mark.parametrize('bc_type',
+                             ['not-a-knot', 'periodic', 'natural', 'clamped'])
+    @pytest.mark.parametrize('axis', [0, 1, 2])
+    @pytest.mark.parametrize('klass', ['make_interp_spline', ])
+    def test_zero_size(self, xp, scp, klass, y_shape, bc_type, axis):
+        x = xp.arange(10)
+        y = xp.zeros(y_shape)
+        xval = xp.arange(3)
+
+        cls = getattr(scp.interpolate, klass)
+        obj = cls(x, y, bc_type=bc_type)
+        assert obj(xval).size == 0
+        assert obj(xval).shape == xval.shape + y.shape[1:]
+
+        # Also check with an explicit non-default axis
+        yt = xp.moveaxis(y, 0, axis)  # (10, 0, 5) --> (0, 10, 5) if axis=1 etc
+
+        obj = cls(x, yt, bc_type=bc_type, axis=axis)
+        sh = yt.shape[:axis] + (xval.size, ) + yt.shape[axis+1:]
+        assert obj(xval).size == 0
+        assert obj(xval).shape == sh
+
+    @testing.numpy_cupy_allclose(scipy_name='scp')
+    @pytest.mark.parametrize('y_shape', [(10, 0, 5), (10, 5, 0)])
     @pytest.mark.parametrize('axis', [0, 1, 2])
     @pytest.mark.parametrize('klass', ['PchipInterpolator',
                                        'Akima1DInterpolator'])
-    def test_zero_size_2(self, xp, scp, cls, y, axis):
+    def test_zero_size_2(self, xp, scp, klass, y_shape, axis):
         x = xp.arange(10)
+        y = xp.zeros(y_shape)
         xval = xp.arange(3)
 
         cls = getattr(scp.interpolate, klass)
@@ -594,7 +620,7 @@ class TestZeroSizeArrays:
         assert obj(xval).shape == xval.shape + y.shape[1:]
 
         # Also check with an explicit non-default axis
-        yt = np.moveaxis(y, 0, axis)  # (10, 0, 5) --> (0, 10, 5) if axis=1 etc
+        yt = xp.moveaxis(y, 0, axis)  # (10, 0, 5) --> (0, 10, 5) if axis=1 etc
 
         obj = cls(x, yt, axis=axis)
         sh = yt.shape[:axis] + (xval.size, ) + yt.shape[axis+1:]
